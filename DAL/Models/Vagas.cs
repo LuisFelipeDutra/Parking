c9bc81846bec5c089348334d2593f78bbcefbde6
Luis Felipe da Silva Dutra <l.felipedutra@live.com>
2017-06-24 22:17:09 -0300

    Login Identity

--- DAL/Models/Vagas.cs
+++ DAL/Models/Vagas.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DAL.Models
 {
-    class Vagas
+    public class Vagas
     {
+        [Key]
+        public int CodigoID { get; set; }
+        [Required]
+        public bool Ocupada { get; set; } //True para sim e false para não
     }
 }
--- MVC/Models/IdentityModels.cs
+++ MVC/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace MVC.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("ParkingContexto", throwIfV1Schema: false)
         {
         }
 
